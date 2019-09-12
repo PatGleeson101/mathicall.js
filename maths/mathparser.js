673b7bee0f1091b5163cd2ccbe72b7073f6519df
Pat Gleeson <patrickgleeson101@gmail.com>
2019-09-12 14:56:48 +1000

    Evaluate BOMDAS
    Integrated tokenisation, conversion to postfix notation and evaluation into a single function to maximise overall speed. Parser can now evaluate expressions according to BOMDAS, but not with variables or functions.

--- maths/mathparser.js
+++ maths/mathparser.js
@@ -5,114 +5,219 @@ A module to parse string maths expressions into usable forms
 var mathParser = (function() {
 	"use strict";
 
-	//Setup allowed characters and tokens
+	//Define and categorise recognised tokens (single characters or multiple letters)
 	let digits = ['0','1','2','3','4','5','6','7','8','9'];
-	let letters = ['a','b','c','d','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	let letters = ['a','b','c','d','h','i','j','k','l','m',
+				   'n','o','p','q','r','s','t','u','v','w','x','y','z',
+				   'A','B','C','D','E','F','G','H','I','J','K','L','M',
+				   'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+	
 	let operators = ['+','-','*','/','^'];
-	let brackets = ['(',')'];
-	let separators = [','];
+	let opFuncs = [];
+
+	let specials = ['(',')',',']; //To keep track of characters with explicitly-coded functions
+
 	let functions = ['sin','cos','abs','max','min'];
 	let actions = [];
 
+	let precedences = {'+':0,
+					   '-':0,
+					   '*':1,
+					   '/':1,
+					   '^':2};
+
 	//Setup token recognition (outside of tokenize function so that pushToken can be defined)
-	let tokenStruct;
+	let actionStack;
+	let valueStack;
 	let token;
 	let tokenType;
 
-	function pushCurrentToken() {
-		if (tokenType === 'letters') {
-			if (functions.includes(token)) {
-				tokenStruct.push(token);
-			} else {
-				for (let i = 0; i < token.length-1; i++) {
-					tokenStruct.push(token[i]);
-					tokenStruct.push('*');
-				}
-				tokenStruct.push(token[token.length-1]);
-			}
-		} else if (token === '') {
-
-		} else {
-			tokenStruct.push(token);
+	function useOperator(operator) { //Currently nonfunctional for variables
+		if (operator === '+') {
+			valueStack.push(valueStack.pop()+valueStack.pop());
+		} else if (operator === '-') {
+			valueStack.push(-1*valueStack.pop()+valueStack.pop());
+		} else if (operator === '*') {
+			valueStack.push(valueStack.pop()*valueStack.pop());
+		} else if (operator === '/') {
+			valueStack.push(1/valueStack.pop()*valueStack.pop());
+		} else if (operator === '^') {
+			let exponent = valueStack.pop();
+			valueStack.push(Math.pow(valueStack.pop(),exponent));
 		}
+	}
+
+	function pushOperator(operator) {
+		let tokPrec = precedences[operator];
+		for (let i = actionStack.length-1; i >= 0; i--) { //Iterate backwards over actionStack
+			if (tokPrec <= precedences[actionStack[i]]) {
+				useOperator(actionStack.pop());
+			} else {
+				break;
+			}
+		}
+		actionStack.push(operator);
+	}
+
+	function pushToken(tok, tokType) {
+		if (tokType === 'letters') { //If token is made of letters:
+			if (functions.includes(tok)) { //If token is a function:
+				actionStack.push(tok); //ADD FUNCTION HERE
+			} else { //Token must be a sequence of implicitly-multiplied variables:
+				for (let i = 0; i < tok.length-1; i++) {
+					valueStack.push(tok[i]);
+					actionStack.push('*');
+				}
+				valueStack.push(tok[tok.length-1]);
+			}
+		} else if (tokType === 'int') {
+			valueStack.push(parseInt(tok));
+		} else if (tokType === 'float') {
+			valueStack.push(parseFloat(tok));
+		} else if (tokType === 'operator') {
+			pushOperator(tok);
+		} else if (tokType === null) {
+			//Do nothing; no token was pushed
+		} else {
+			throw 'Invalid token type';
+		}
+
 		tokenType = null;
 		token = '';
 	}
 	
-	function tokenizeMathExp(string) {
+	function evalExp(string, mode = 'literal') {
 		//Setup input string
 		let inputExp = string.replace(" ","");
 		let len = inputExp.length;
 
-		//Setup token storage
-		tokenStruct = [];
+		//Clear old token storage
+		valueStack = [];
+		actionStack = [];
 		token = '';
 		tokenType = null;
 
-		for (let i = 0; i < len; i++) {
-			let char = inputExp[i];
+		//Special storage
+		let wasOperator = false;
 
-			if (digits.includes(char)) { //Check if character is a digit
+		//Iterate over each character in input expression
+		for (let i = 0; i < len; i++) {
+			let char = inputExp[i]; //Get character
+
+			//Numerical checks
+			if (digits.includes(char)) { //If character is a digit:
+				wasOperator = false;
 				//Add to current token, if valid
-				if (tokenType === null) {
-					tokenType = 'number';
+				if (tokenType === null) { //No existing token:
+					tokenType = 'int';
 					token += char;
-				} else if (tokenType === 'number') {
+				} else if ((tokenType === 'int')||(tokenType = 'float')) {//Existing numerical token:
 					token += char;
-				} else {
-					throw 'Invalid maths expression: contains misplaced digit';
+				} else { //Digit is invalid
+					throw 'Invalid maths expression: contains miscellaneous digit';
+				}
+			} else if (char === '.') { //If character is a decimal point:
+				if (tokenType === null) { //No existing token:
+					tokenType = 'float';
+					token += '0.'; //Begin new float with a 0.
+				} else if (tokenType === 'int') {//Existing integer token:
+					tokenType = 'float';
+					token += char;
+				} else { //Decimal point is invalid
+					throw 'Invalid maths expression: contains miscellaneous decimal point';
 				}
 
+			//Variable & Function checks:
 			} else if (letters.includes(char)) { //Check if character is a letter
+				wasOperator = false;
 				//Add to current token if valid
 				if (tokenType === null) {
 					tokenType = 'letters';
 					token += char;
 				} else if (tokenType === 'letters') {
 					token += char;
-				} else { //Assume implicit multiplication
-					pushCurrentToken();
-					tokenStruct.push('*');
-					tokenType = 'letters';
+				} else { //Assume implicit multiplication between current token and new letter token
+					pushToken(token, tokenType); //Push current token
+					pushToken('*', 'operator'); //Push * operator to actionStack
+					tokenType = 'letters'; //Begin new letter-based token
 					token = char;
 				}
 
+			//Operator checks
 			} else if (operators.includes(char)) {
-				pushCurrentToken();
-				tokenStruct.push(char);
+				wasOperator = true;
+				pushToken(token, tokenType);
+				pushOperator(char);
 
-			} else if (char === '(') { //Check if character is a left bracket
-				pushCurrentToken();
-				if (!functions.includes(tokenStruct[tokenStruct.length-1])) {
-					tokenStruct.push('*');
+			//Special checks: separators & brackets
+			} else if (char === '(') { //If character is an opening bracket:
+				pushToken(token, tokenType);
+				if ((!functions.includes(valueStack[valueStack.length-1]))&&(i>0)&&(!wasOperator)) { //If previous token exists and is not a function
+					pushOperator('*');
 				} 
-				tokenStruct.push('(');
+				actionStack.push('(');
+				wasOperator = true;
 
-			} else if (char === ')') {
-				pushCurrentToken();
-				tokenStruct.push(')');
+			} else if (char === ')') { //If character is a closing bracket:
+				pushToken(token, tokenType);
+				for (let i = actionStack.length-1; i >= 0; i--) { //Iterate backwards over actionStack
+					let action = actionStack.pop(); //Currently risky: won't catch errors (misplacecd brackets)
+					if (action === '(') {
+						break;
+					} else {
+						useOperator(action);
+					}
+				}
 
-			} else if (separators.includes(char)) {
-				pushCurrentToken();
-				tokenStruct.push(char);
+			} else if (separators.includes(char)) { //If character is a comma (separator):
+				pushToken(token, tokenType);
+				actionStack.push(char);
+				wasOperator = true;
 
+			//Character is not recognised:
 			} else {
 				throw 'Invalid maths expression: contains invalid character';
 			}
 		}
+
 		//Push final token
-		pushCurrentToken();
+		pushToken(token, tokenType);
 
-		return tokenStruct;
+		//Use remaining operators
+		for (let i = actionStack.length-1; i >= 0; i--) { //Iterate backwards over actionStack
+			useOperator(actionStack.pop());
+		}
+
+		return [valueStack, actionStack];
 	}
 
-	function tokensToPostfixNotation(tokenStruct) {
+	function tokensToPostfixNotation(valueStack) {
+		let actionStack = [];
+		let postfixStruct = [];
 
+		let len = valueStack.length;
+		for (let i = 0; i < len; i++) {
+			let tok = valueStack[i];
+			if (functions.includes(tok)) {
+				//Code if it's a function
+			} else if (operators.includes(tok)) {
+				
+			} else if (separators.includes(tok)) {
+				//Code if it's a separator
+			} else if (brackets.includes(tok)) {
+				//Code if it's a bracket
+			} else {
+				//Must be a literal or variable
+				postfixStruct.push(tok);
+			}
+		}
+		//Add remaining operators
+		for (let i = actionStack.length-1; i >= 0; i--) {
+			postfixStruct.push(actionStack.pop());
+		}
+
+		return postfixStruct;
 	}
 
-	function evalPostfixStruct() {
-
-	}
-
-	return {tokenize: tokenizeMathExp}
+	return {eval: evalExp};
 }());
